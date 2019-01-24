e4f032b7220a83602a3a04897dae1cd852bc6fce
p00582564 <46765708+p00582564@users.noreply.github.com>
2019-01-23 19:44:46 -0500

    GroupProj

---
+++ GroupProj.java
@@ -0,0 +1,21 @@
+public class GroupProj{
+  public static void main (String [] args){
+    Box b1 = new Box;
+    
+    System.out.println("box area is" + b1.area() );
+    
+  }
+  
+  public class Box{
+    private double sideLength;
+    
+    public static area{
+       return sideLength*sideLength;
+    
+    public setSideLength (double length){
+      sideLength = length;
+    
+    public getSideLenth{
+      return sideLength;
+  }
+}
