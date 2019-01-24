6184312ee251a863ce2a436ee90b594e20e63864
p00582564 <46765708+p00582564@users.noreply.github.com>
2019-01-23 21:17:41 -0500

    Update GroupProj.java

--- GroupProj.java
+++ GroupProj.java
@@ -1,48 +1,48 @@
 //Cody, Tony, Themba, Carmen
 
-public class GroupProj {
-	public static void main (String [] args){
-		
+public class GrouProj {
+
+	public static void main(String[] args) {
+		Box test1 = new Box();
+		// no closing instance of type is available
 	} // end of main
+}
 
-	public class Box{
-		private double length, width;
-    
-		Box(){
-			length = 1;
-			width = 1;
-		}
-    
-		Box(double inpLength, double inpWidth){
-			length = inpLength;
-			width = inpWidth;
-		}
-    
-		public void setLength(double newLength){
-			length = newLength;
-		}
-    
-		public void setWidth (double newWidth){
-			width = newWidth;
-		}
-    
-		public double getLength() {
-			return length;
-		}
-    
-		public double getWidth(){
-			return width;
-		}
-   
-		public double getArea(){
-			return length*width;
-		}
-    
-		public double getPerimeter(){
-			return 2*length+2*width;
-		}  
-    
-		
-  }//end of Box
+class Box {
+	private double length, width;
 
-}// end
+	Box() {
+		length = 1;
+		width = 1;
+	}
+
+	Box(double inpLength, double inpWidth) {
+		length = inpLength;
+		width = inpWidth;
+	}
+
+	void setLength(double newLength) {
+		length = newLength;
+	}
+
+	void setWidth(double newWidth) {
+		width = newWidth;
+	}
+
+	double getLength() {
+		return length;
+	}
+
+	double getWidth() {
+		return width;
+	}
+
+	double getArea() {
+		return length * width;
+	}
+
+	double getPerimeter() {
+		return 2 * length + 2 * width;
+	}
+
+}// end of Box
