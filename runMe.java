64d376b13517b200bcdb240042b32b8373e98fc4
Cody D'Orazio <44353948+dcmdorazio@users.noreply.github.com>
2019-01-30 09:58:26 +0700

    Add files via upload
    Hey guys. Just an update this is the version I have been working on for our project. It should be done tomorrow and will have more functionality. First off the first window that pops up in the top left corner of the window is a message Pane from JOptionpane. It disappears within 10 seconds and prompts another box with user interactivity and will soon take input and spit out an answer. Take a look! (as of right now it will not give an answer so just wait I should have that up and running by tomorrow!)
    
    **There is a main method file and a method body file in this upload**

---
+++ runMe.java
@@ -0,0 +1,18 @@
+// Collaborators: Cody, Tony, Themba, Carmen
+// Assignment 1 (Group 1): an example on the concept of encapsulation
+
+public class runMe {
+
+
+   public static void main(java.lang.String[] args) {
+      /*
+      GroupProj.Box test1 = new GroupProj.Box();
+      System.out.println("length is " + test1.getLength());*/
+
+      GroupProj r = new GroupProj();
+      r.OptionPaneUI();
+
+
+
+   }
+}
